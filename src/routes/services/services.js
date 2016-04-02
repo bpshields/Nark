94d2fc0919ddf73ccbbe8b3a52ac2435bf8888e9
Brantley Shields <brantley.shields@gmail.com>
2016-04-02 19:29:35 -0400

    Initial CRUD operations implemented for services, bare minimum validation but everything works

--- src/routes/services/services.js
+++ src/routes/services/services.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var r = require("rethinkdb");
 
+//TODO: Mask error messages as they expose too much data to users (table names, etc)
+
 // middleware that is specific to this router
 // setting Content-Type for all of the responses
 router.use(function timeLog(req, res, next) {
@@ -9,7 +11,6 @@ router.use(function timeLog(req, res, next) {
     next();
 });
 
-// define the home page route
 router.get('/services', function(req, res) {
     r.db('nark').table('services').run(req.app.locals.db_connection, function(err, cursor) {
         if (err) throw err;
@@ -20,30 +21,113 @@ router.get('/services', function(req, res) {
     });
 });
 
+router.get('/services/:id', function(req, res) {
+    var response = null;
+
+    //If Validation passes lets insert the service into the database
+    r.db('nark').table('services').get(req.params.id).run(req.app.locals.db_connection, function(err, result) {
+        if (err) {
+            res.status(500);
+            response = err;
+        } else {
+            res.status(200);
+            response = result;
+        }
+        res.json(response);
+    })
+});
+
 
 //Required Properties: Host,Path,Type,PrefResponseTime,PrefStatusCode,PrefResponseData
-//TODO: Refactor this in the morning and actually put more thought into validation
 router.post('/services/create', function(req, res) {
     var service = req.body;
     var response = null;
-    if ( validateBody(service) ) {
-        r.db('nark').table('services').insert(service).run(req.app.locals.db_connection, function(err, result) {
-            if (err) {
-                res.status(500);
-                response = err;
-            }
+
+    //Validate required parameters
+    if ( !service.hostname ) {response = {"ErrorMessage": "Invalid hostname"}; res.status(500); res.json(response);}
+    if ( !service.path ) {response = {"ErrorMessage": "Invalid path"}; res.status(500); res.json(response);}
+    if ( !service.method ) {response = {"ErrorMessage": "Invalid method"}; res.status(500); res.json(response);}
+
+
+    //If Validation passes lets insert the service into the database
+    r.db('nark').table('services').insert(service).run(req.app.locals.db_connection, function(err, result) {
+        if (err) {
+            res.status(500);
+            response = err;
+        } else {
+            res.status(200);
             response = result;
-        })
-        res.status(200);
-    }
-    res.json(response);
+        }
+        res.json(response);
+    })
 });
 
-function validateBody(body) {
-    var valid = true;
-    console.log(body);
-    return valid;
-}
+//GET DELETE
+router.get('/services/delete/:id', function(req, res) {
+    var response = null;
+
+    //If Validation passes lets insert the service into the database
+    r.db('nark').table('services').get(req.params.id).delete().run(req.app.locals.db_connection, function(err, result) {
+        if (err) {
+            res.status(500);
+            response = err;
+        } else {
+            res.status(200);
+            response = result;
+        }
+        res.json(response);
+    })
+});
+
+/**
+ * Route to delete a service object from database using post. Requires a valid json {"id":"id-here"}
+ */
+router.post('/services/delete', function(req, res) {
+    var service = req.body;
+    var response = null;
+
+    //Validate required parameters
+    if ( !service.id ) {response = {"ErrorMessage": "Invalid id"}; res.status(500); res.json(response);}
+
+    //If Validation passes lets insert the service into the database
+    r.db('nark').table('services').get(service.id).delete().run(req.app.locals.db_connection, function(err, result) {
+        if (err) {
+            res.status(500);
+            response = err;
+        } else {
+            res.status(200);
+            response = result;
+        }
+        res.json(response);
+    })
+});
+
+/**
+ * Route to update a service object in the database using post. Requires a valid json
+ */
+router.post('/services/update', function(req, res) {
+    var service = req.body;
+    var response = null;
+
+    //Validate required parameters
+    if ( !service.id ) {response = {"ErrorMessage": "Invalid id"}; res.status(500); res.json(response);}
+    if ( !service.hostname ) {response = {"ErrorMessage": "Invalid hostname"}; res.status(500); res.json(response);}
+    if ( !service.path ) {response = {"ErrorMessage": "Invalid path"}; res.status(500); res.json(response);}
+    if ( !service.method ) {response = {"ErrorMessage": "Invalid method"}; res.status(500); res.json(response);}
+
+    //If Validation passes lets insert the service into the database
+    r.db('nark').table('services').get(service.id).replace(service).run(req.app.locals.db_connection, function(err, result) {
+        if (err) {
+            res.status(500);
+            response = err;
+        } else {
+            res.status(200);
+            response = result;
+        }
+        res.json(response);
+    })
+});
+
 
 
 module.exports = router;
